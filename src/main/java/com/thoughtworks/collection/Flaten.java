9bbebb519b487c9cd269864ee2e323a9cf34f91f
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-11 07:13:27 +0800

    Committed completed task for Flatten Test

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,10 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
@@ -12,10 +15,18 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> finalList = new ArrayList<>();
+        for (Integer[] currentArray: array) {
+            Arrays.stream(currentArray).forEach(finalList::add);
+        }
+        return finalList;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> finalList = new ArrayList<>();
+        for (Integer[] currentArray: array) {
+            Arrays.stream(currentArray).forEach(finalList::add);
+        }
+        return finalList.stream().distinct().collect(Collectors.toList());
     }
 }
